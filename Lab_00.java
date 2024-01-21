601d4f864b15ae9e80e6ed30879890dd43299e3f
Maximus Fernandez <143644768+maximusf@users.noreply.github.com>
2024-01-21 03:30:49 -0500

    Add files via upload

---
+++ Lab_00.java
@@ -0,0 +1,12 @@
+/*
+	Maximus Fernandez
+	Due: 01/26/2024 11:55PM
+*/
+public class Lab_00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lab_01.java
@@ -0,0 +1,12 @@
+/*
+	Maximus Fernandez
+	Due: 02/02/2024 11:55PM
+*/
+public class Lab_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
