2460d4d9940e33109763ce6f52b5230af8d809fc
Maximus Fernandez <143644768+maximusf@users.noreply.github.com>
2024-02-18 12:15:50 -0500

    Add files via upload

---
+++ Homework03/TaskManager.java
@@ -0,0 +1,22 @@
+/*
+* Written by Maximus Fernandez
+* Homework 03
+* Task Organizer!
+* Due 02/23/2024 by 11:55PM
+*/
+public class TaskManager {
+
+	
+	/*
+	 *  Needs to add/remove tasks
+	 *  
+	 *  Print tasks to console
+	 * 	Print tasks file
+	 *  Read tasks file
+	 *  
+	 *  
+	 *  
+	 *  
+	 */
+	
+}
---
+++ Homework03/TaskManagerFE.java
@@ -0,0 +1,13 @@
+/*
+* Written by Maximus Fernandez
+* Homework 03
+* Task Organizer!
+* Due 02/23/2024 by 11:55PM
+*/
+public class TaskManagerFE {
+
+	
+	public static void main(String[] args) {
+		
+	}
+}
